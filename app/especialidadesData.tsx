aee60f8c00f8a7712e1d90a45e0614e082ef1171
Victor Luiz <victorluiz.idv@gmail.com>
2026-06-16 18:29:53 -0400

    Melhorar icones de cardiologia (coracao+batimento) e ortopedia (osso)

--- app/especialidadesData.tsx
+++ app/especialidadesData.tsx
@@ -44,7 +44,11 @@ export const especialidades: Especialidade[] = [
     ],
     icone: (
       <svg {...svg}>
-        <path pathLength={1} d="M2 12h4l2-6 4 12 2-6h8" />
+        <path
+          pathLength={1}
+          d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
+        />
+        <path pathLength={1} d="M3.22 12H9.5l.5-1 2 4.5 2-7 1.5 3.5h5.27" />
       </svg>
     ),
   },
@@ -83,7 +87,10 @@ export const especialidades: Especialidade[] = [
     ],
     icone: (
       <svg {...svg}>
-        <path pathLength={1} d="M9 9a2 2 0 1 0-2-2 2 2 0 1 0-1 3l7 7a2 2 0 1 0 1 2 2 2 0 1 0 2-1z" />
+        <path
+          pathLength={1}
+          d="M17 10c.7-.7 1.69 0 2.5 0a2.5 2.5 0 1 0 0-5 .5.5 0 0 1-.5-.5 2.5 2.5 0 1 0-5 0c0 .81.7 1.8 0 2.5l-7 7c-.7.7-1.69 0-2.5 0a2.5 2.5 0 0 0 0 5c.28 0 .5.22.5.5a2.5 2.5 0 1 0 5 0c0-.81-.7-1.8 0-2.5Z"
+        />
       </svg>
     ),
   },
